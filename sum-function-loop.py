379bf1ffce2563350f9351b3071cb8c11f225f06
Anastassia Fugier <94131859+anastassiafugier@users.noreply.github.com>
2021-11-24 21:52:06 +0200

    For loop python
    * Renamed variables
    
    * Fixed let/const
    
    * Sum function python
    
    * For loop python
    
    Co-authored-by: Anastasia <anastasia@Anastasias-MacBook-Air.local>

--- sum-function-loop.py
+++ sum-function-loop.py
@@ -8,3 +8,18 @@ def sum_of_num(first_num, second_num):
 
 # Usage
 sum_of_num(13, 42)
+
+
+# For loop
+# Implementation
+def sum_loop(first_num, second_num):
+    arguments = [first_num, second_num]
+    num = 0
+    for i in arguments:
+        num += i
+    print(num)
+    return num
+
+
+# Usage
+sum_loop(2, 38)
